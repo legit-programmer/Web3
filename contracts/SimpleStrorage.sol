013ce488c27d22ea926a124154a40e642ba2998d
legit-programmer <project.purpose.rollno.29@gmail.com>
2023-06-07 22:48:36 +0530

    simple contract with data types with example

---
+++ contracts/SimpleStrorage.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.8;
+
+
+contract SimpleStorage {
+    bool onOff = true;
+    int256 number = -4654;
+    uint256 numberUnsigned = 64;
+    string name = "Test Name";
+    bytes32 nameInBytes = "Byte Name";
+    address mySepolianEthAdd = 0x7F271cb4EeA76990c8B3aFDB882F3e0751C9676A;
+    
+}
