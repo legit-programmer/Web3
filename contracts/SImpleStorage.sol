97a975e5a7498a2d353407686f796c73af9306a7
legit-programmer <project.purpose.rollno.29@gmail.com>
2023-06-08 22:23:57 +0530

    structures, view and pure functions

--- contracts/SImpleStorage.sol
+++ contracts/SImpleStorage.sol
@@ -3,9 +3,27 @@ pragma solidity >=0.8.8;
 
 contract SimpleStorage {
     uint256 public  number;
+    Person public person1 = Person({name:"NAme", age:45});
 
+    struct Person{
+        string name;
+        uint256 age;
+    }
+
+    Person[] public people;
     function getNumber (uint256 _number) public {
         number = _number;
+    }
+    function retrieve() public view returns(uint256){
+        return number;
+    }
+
+    function pureRetrieve() public pure returns(uint256){
+        return 1+1;
+    }
+
+    function pushToPeople(string memory _name, uint256 _age) public{
+        people.push(Person(_name, _age));
     }
 }
 
