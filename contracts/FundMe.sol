828070f31d4098986ad6c59dac1a87bdc0cfe7d2
legit-programmer <project.purpose.rollno.29@gmail.com>
2023-06-14 07:19:53 +0530

    working with libraries

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,38 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "contracts/PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
+
     uint256 minUSD = 50;
-    AggregatorV3Interface internal priceFeed;
     address[] public funders;
     mapping(address=>uint256) amounts;
 
-    constructor() {
-        priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-    } 
+    
 
     function fund() public payable {
-        require(getConversionRate(msg.value)>=minUSD, "Didn't send enough");
+        require(msg.value.getConversionRate()>=minUSD, "Didn't send enough");
         funders.push(msg.sender);
         amounts[msg.sender] = msg.value;
     }
 
-    function getPrice() public view returns (uint256) {
-        (
-            /* uint80 roundID */,
-            int answer,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
-            /*uint80 answeredInRound*/
-        ) = priceFeed.latestRoundData();
-
-        return uint256(answer*1e10);
-    }
-
-    function getConversionRate(uint256 amountInEth) public view returns (uint256) {
-        return ((amountInEth*getPrice())/1e18);
-    }
+    
     function withdraw() private{
 
     }
---
+++ contracts/PriceConverter.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    function getPrice() internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (
+            /* uint80 roundID */,
+            int answer,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+
+        return uint256(answer*1e10);
+    }
+
+    function getConversionRate(uint256 amountInEth) internal view returns (uint256) {
+        return ((amountInEth*getPrice())/1e18);
+    }
+}
