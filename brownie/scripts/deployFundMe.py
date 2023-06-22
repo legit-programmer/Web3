ebb46cd52fdfa2e73842c273c5c27f52111a820c
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-06-22 06:12:20 +0530

    ..x2

--- brownie/scripts/deployFundMe.py
+++ brownie/scripts/deployFundMe.py
@@ -3,7 +3,7 @@ from.helpful_scripts import get_account
 
 def deploy():
     account = get_account()
-    FundMe.deploy({'from':account}, publish_source=True)
+    FundMe.deploy('0x694AA1769357215DE4FAC081bf1f309aDC325306', {'from':account}, publish_source=True)
     
 def main():
     network.priority_fee('1.5 gwei')
